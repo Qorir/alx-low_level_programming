d9343c13beabca30f4b2ce8cf1ad8336c909aec3
Qorir <evanskorir1677@gmail.com>
2022-12-16 12:07:20 -0800

    task1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,14 @@
+#include "main.h"
+
 /**
  * _isupper - check if c is upper
  * @c: input for the alphabet
  * Return: 1 if uppercase,0 if not
  */
 
-int _isupper(int c)
+char _isupper(char c)
 {
-if (c >= 65 && c <= 90)
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
