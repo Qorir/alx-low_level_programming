bc47b3b1b2531b7957107877bee60e17861bc17e
Qorir <evanskorir1677@gmail.com>
2022-12-19 00:52:11 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of n to 98
+ * @n: integer pointer to variable to be updated
+ *
+ * Return: 0
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98
+}
