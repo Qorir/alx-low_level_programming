000d18e3b1a427cd13d3a27f9aaceaf693608d07
Qorir <evanskorir1677@gmail.com>
2022-12-23 10:10:59 -0800

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@
  * DESCRIPTION: an header file containing prototypes used in 0x06_pointers_arrays_strings
  */
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
