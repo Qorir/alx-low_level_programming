4ffde99ebbab57dc90a46f2dcb3bc160574dcc47
Qorir <evanskorir1677@gmail.com>
2023-01-01 11:19:14 -0800

    task7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,13 +3,14 @@
 /**
  * cap_string - capitalises all words of string
  * @str - string to be capitalised
+ * Description: capitalises string
  * Return: a pointer to the changed string
  */
 
 char *cap_string(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
