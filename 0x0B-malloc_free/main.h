907307b5e0b0913aa6876657c14beefe737e02ef
Qorir <evanskorir1677@gmail.com>
2023-01-11 13:27:24 -0800

    task 2

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 /**
- * header file containing all functions used in this directory
+ * header for functions used in this directory
  */
+
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
